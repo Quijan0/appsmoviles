91c97541fccfbe481d05cfeb2b0896f64239402c
Quijan0 <Quijanuxx@gmail.com>
2026-09-02 00:43:12 -0500

    a
    a

--- guias y clases/project_mobile/project_mobile/comunicacion-2/src/app/app.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { ContadorComponent } from './componentes/contador/contador';
-@Component({
- selector: 'app-root',
- standalone: true,
- imports: [ContadorComponent],
- template: '<app-contador />'
-})
-export class App {}
